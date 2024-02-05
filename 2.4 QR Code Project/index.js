ef69d272a4c1f1fe9e7cb606f045866be793c67b
Jonas Moons <moons_jonas@outlook.com>
2024-02-05 16:18:18 +0100

    add qr code generator with node.js

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -5,13 +5,17 @@
 */
 import inquirer from 'inquirer';
 import qr from 'qr-image';
+import fs from 'fs';
 
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+  .prompt([{
+    message: "Type in your URL: ",
+    name: "URL",
+  }])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
